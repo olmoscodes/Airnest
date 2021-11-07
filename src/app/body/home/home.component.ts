e2fd878b767b8efa3d1eb9a57ba1dc577f2f411a
marc <olmoscodes@gmail.com>
2021-11-07 22:06:26 +0100

    home, navbar, and footer created with animations

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    gsap.registerPlugin(ScrollTrigger);
 
+    if (window.matchMedia('(min-width: 400px)').matches) {
+      const home1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.hm1',
+          start: 'top 0%',
+          end: '+=1300',
+          pin: true,
+          scrub: 1,
+          markers: false,
+        },
+      });
+
+      home1.fromTo('.hc1', { x: '100vw' }, { x: '-100vw' });
+    } else {
+      const home1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.hm1',
+          start: 'top 5px',
+          end: '+=1700',
+          pin: true,
+          scrub: 1,
+          markers: false,
+        },
+      });
+
+      home1.fromTo('.hc1', { x: '100vw' }, { x: '-650vw' });
+    }
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +14,71 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    gsap.registerPlugin(ScrollTrigger);
+  
+    const navbarHideTL = gsap.timeline({paused: true})
+      
+    navbarHideTL.to("#nav", {y: -100})
+    
+    let hamOpen = document.getElementById("ham-open")!;
+    let hamClose = document.getElementById("ham-close")!;
+  
+    const navbarShowTL = gsap.timeline({paused: true})
+  
+    navbarShowTL.fromTo(".hamburguer-nav", {opacity: 0, y: -200}, { opacity: 1, y: 0})
+    .to("#ham-line-1-nav", {rotation: "45", y: 8}, "-=0.5")
+    .to("#ham-line-2-nav", {opacity: 0}, "-=0.5")
+    .to("#ham-line-3-nav", {rotation: "-45", y: -8}, "-=0.5")
+  
+    hamOpen.addEventListener("click", function () {
+      console.log("nav opened");
+      hamOpen.style.pointerEvents= "none";
+      hamClose.style.pointerEvents= "inherit";
+      hamOpen.style.opacity = "0";
+      hamOpen.style.opacity = "1";
+      navbarShowTL.play();
+    }, false);
+  
+    hamClose.addEventListener("click", function () {
+      console.log("nav closed");
+      hamOpen.style.pointerEvents= "inherit";
+      hamClose.style.pointerEvents= "none";
+      hamOpen.style.opacity = "1";
+      hamOpen.style.opacity = "0";
+      navbarShowTL.reverse();
+    }, false);
+  
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if(currentScrollPos > 176) {
+              if (prevScrollpos > currentScrollPos) {
+            navbarHideTL.reverse();		
+            } else {
+                navbarHideTL.play();
+            }
+        }
+        prevScrollpos = currentScrollPos;
+      }
+    
+    const navbarTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.trigger',
+        start: 'bottom 50%',
+        end: 'bottom 40%',
+        scrub: true,
+        markers: false
+      },
+    });
+    
+    navbarTL.fromTo( '#nav', { background: 'transparent' }, { backgroundColor: 'white' })
+    .to(".nav-button", {color: "#2D2D2D"}, "-=0.5")
+    .fromTo(".airnest-logo-white", {opacity: 1}, {opacity: 0}, "-=0.5")
+    .fromTo(".airnest-logo-black", {opacity: 0}, {opacity: 1}, "-=0.5")
+    .fromTo(".livebox", {backgroundColor: 'transparent'}, {backgroundColor: 'white'}, "-=0.5")
+    
+  
   }
 
 }
