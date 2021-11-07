021c29907874a02668da41215f43cf39cf8b4c3b
marc <olmoscodes@gmail.com>
2021-11-07 22:33:22 +0100

    migrating our-nests landing page

--- src/app/body/our-nests/our-nests.component.ts
+++ src/app/body/our-nests/our-nests.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-our-nests',
   templateUrl: './our-nests.component.html',
-  styleUrls: ['./our-nests.component.css']
+  styleUrls: ['./our-nests.component.css'],
 })
 export class OurNestsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    gsap.registerPlugin(ScrollTrigger);
 
+    if (window.matchMedia('(min-width: 400px)').matches) {
+      ScrollTrigger.batch('.pp', {
+        onEnter: (batch) => gsap.to(batch, { opacity: 1, stagger: 0.1 }),
+        onLeave: (batch) => gsap.to(batch, { opacity: 0, stagger: 0.1 }),
+        onEnterBack: (batch) =>
+          gsap.to(batch, { opacity: 1, stagger: 0.15, overwrite: true }),
+        onLeaveBack: (batch) =>
+          gsap.set(batch, { opacity: 0, overwrite: true }),
+      });
+    } else {
+      ScrollTrigger.batch('.pp', {
+        onEnter: (batch) => gsap.to(batch, { opacity: 1, stagger: 0.1 }),
+        onLeave: (batch) => gsap.to(batch, { opacity: 0, stagger: 0.1 }),
+        onEnterBack: (batch) =>
+          gsap.to(batch, { opacity: 1, stagger: 0.15, overwrite: true }),
+        onLeaveBack: (batch) =>
+          gsap.set(batch, { opacity: 0, overwrite: true }),
+      });
+    }
+  }
 }
